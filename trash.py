8b3d4ecd2dffa258e802efc82eb36eaf4026c1c5
muhammad ali <muhammadaliog3@live.com>
2020-11-26 21:42:00 -0500

    poor solution

--- trash.py
+++ trash.py
@@ -1,3 +1,5 @@
 
-import yolov5
+from yolov5 import *
+import yolov5.models as models
+help(models)
 from yolov5.detect import detect
--- yolov5/models/common.py
+++ yolov5/models/common.py
@@ -5,8 +5,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-from utils.datasets import letterbox
-from utils.general import non_max_suppression, make_divisible, scale_coords
+from yolov5.utils.datasets import letterbox
+from yolov5.utils.general import non_max_suppression, make_divisible, scale_coords
 
 
 def autopad(k, p=None):  # kernel, padding
