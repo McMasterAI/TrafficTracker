26cbd890bb95eeff6ffdfee6c5f4c80baef6ca71
muhammad ali <muhammadaliog3@live.com>
2021-01-16 18:03:13 -0500

    Modularizing deep_sort.py

--- deep_sort.py
+++ deep_sort.py
@@ -13,6 +13,34 @@ from deep_sort.detection import Detection
 from deep_sort.tracker import Tracker
 from deep_sort import generate_detections as gdet
 
+# Helpers
+def efficiency_statistics(detection_times,tracking_times):
+    ms = sum(detection_times)/len(detection_times)*1000
+    fps = 1000 / ms
+    fps2 = 1000 / (sum(tracking_times)/len(tracking_times)*1000)
+    return ms,fps,fps2          
+def get_tracker_info(tracker, val_list,key_list):
+    tracked_bboxes = []
+    for track in tracker.tracks:
+        if not track.is_confirmed() or track.time_since_update > 5:
+            continue 
+        bbox = track.to_tlbr() # Get the corrected/predicted bounding box
+        class_name = track.get_class() #Get the class name of particular object
+        tracking_id = track.track_id # Get the ID for the particular track
+        index = key_list[val_list.index(class_name)] # Get predicted object index by object name
+        tracked_bboxes.append(bbox.tolist() + [tracking_id, index]) # Structure data, that we could use it with our draw_bbox function
+    return tracked_bboxes
+def draw_image(original_frame,tracked_bboxes,CLASSES,fps=None):
+    image = draw_bbox(original_frame, tracked_bboxes, CLASSES=CLASSES, tracking=True)
+    if fps !=None:
+        image = cv2.putText(image, "Time: {:.1f} FPS".format(fps), (0, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
+    return image
+def get_video_capture_info(vid):
+    # by default VideoCapture returns float instead of int
+    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(vid.get(cv2.CAP_PROP_FPS))
+    return width,height,fps
 
 def Object_tracking(Yolo, video_path, output_path, input_size=416, show=False, CLASSES=YOLO_COCO_CLASSES, score_threshold=0.3, iou_threshold=0.45, rectangle_colors='', Track_only = []):
     # Definition of the parameters
@@ -25,23 +53,21 @@ def Object_tracking(Yolo, video_path, output_path, input_size=416, show=False, C
     metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
     tracker = Tracker(metric)
 
-    times, times_2 = [], []
 
     if video_path:
         vid = cv2.VideoCapture(video_path) # detect on video
     else:
         vid = cv2.VideoCapture(0) # detect from webcam
-
-    # by default VideoCapture returns float instead of int
-    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(vid.get(cv2.CAP_PROP_FPS))
+    width, height, fps = get_video_capture_info(vid)
     codec = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(output_path, codec, fps, (width, height)) # output_path must be .mp4
 
     NUM_CLASS = read_class_names(CLASSES)
     key_list = list(NUM_CLASS.keys()) 
     val_list = list(NUM_CLASS.values())
+
+    detection_times, tracking_times = [], []
+
     while True:
         _, frame = vid.read()
 
@@ -89,53 +115,40 @@ def Object_tracking(Yolo, video_path, output_path, input_size=416, show=False, C
         names = np.array(names)
         scores = np.array(scores)
         features = np.array(encoder(original_frame, boxes))
-        print(boxes,names,scores,features)
-
-        detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(boxes, scores, names, features)]
 
         # Pass detections to the deepsort object and obtain the track information.
+        detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(boxes, scores, names, features)]
         tracker.predict()
         tracker.update(detections)
 
         # Obtain info from the tracks
-        tracked_bboxes = []
-        for track in tracker.tracks:
-            if not track.is_confirmed() or track.time_since_update > 5:
-                continue 
-            bbox = track.to_tlbr() # Get the corrected/predicted bounding box
-            class_name = track.get_class() #Get the class name of particular object
-            tracking_id = track.track_id # Get the ID for the particular track
-            index = key_list[val_list.index(class_name)] # Get predicted object index by object name
-            tracked_bboxes.append(bbox.tolist() + [tracking_id, index]) # Structure data, that we could use it with our draw_bbox function
+        tracked_bboxes = get_tracker_info(tracker, val_list,key_list)
+        
+        # update the times information
+        t3 = time.time()
+        detection_times.append(t2-t1)
+        tracking_times.append(t3-t1)
+        detection_times = detection_times[-20:]
+        tracking_times = tracking_times[-20:]
+
+        ms, fps, fps2 = efficiency_statistics(detection_times,tracking_times)
 
         # draw detection on frame
         image = draw_bbox(original_frame, tracked_bboxes, CLASSES=CLASSES, tracking=True)
-
-        t3 = time.time()
-        times.append(t2-t1)
-        times_2.append(t3-t1)
-        
-        times = times[-20:]
-        times_2 = times_2[-20:]
-
-        ms = sum(times)/len(times)*1000
-        fps = 1000 / ms
-        fps2 = 1000 / (sum(times_2)/len(times_2)*1000)
-        
         image = cv2.putText(image, "Time: {:.1f} FPS".format(fps), (0, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
 
         # draw original yolo detection
         #image = draw_bbox(image, bboxes, CLASSES=CLASSES, show_label=False, rectangle_colors=rectangle_colors, tracking=True)
 
+        # show and store the results
         print("Time: {:.2f}ms, Detection FPS: {:.1f}, total FPS: {:.1f}".format(ms, fps, fps2))
         if output_path != '': out.write(image)
         if show:
             cv2.imshow('output', image)
-            
             if cv2.waitKey(25) & 0xFF == ord("q"):
                 cv2.destroyAllWindows()
-                break
-            
+                break   
+
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
