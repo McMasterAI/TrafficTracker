50aaaaf3b8baebb1c0e1b1d4339f7fec4c3f0ee3
Connor Sung Czarnuch <connor.czarnuch@icloud.com>
2021-01-18 10:31:45 -0500

    Initial commit of detections.py

---
+++ detections.py
@@ -0,0 +1,284 @@
+from yolov5.utils.torch_utils import select_device, load_classifier, time_synchronized
+from yolov5.utils.general import (
+    check_img_size, non_max_suppression, apply_classifier, scale_coords,
+    xyxy2xywh, plot_one_box, strip_optimizer, set_logging)
+from yolov5.utils.datasets import LoadStreams, LoadImages
+from yolov5.models.experimental import attempt_load, Ensemble
+from yolov5.models.common import Conv
+from yolov5.utils.google_utils import attempt_download
+import yolov5
+import argparse
+import os
+import shutil
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from numpy import random, zeros_like
+
+
+### The two lines below, import sys AND sys.path.insert(0,'./yolov5') , are necessary in order
+### to avoid modules not found errors.
+### https://github.com/ultralytics/yolov5/issues/353
+import sys
+sys.path.insert(0, './yolov5')
+
+
+class Yolov5DeepSort(Ensemble):
+    def __init__(self):
+        super().__init__()
+        self.device = None
+        self.weights = None
+        self.is_half = False
+        self.imgsz = None
+        self.names = []
+        self.colors = [[],[],[]]
+
+
+def attempt_load_yolov5(weights, map_location=None):
+    model = Yolov5DeepSort()
+    # Loads an ensemble of models weights=[a,b,c] or a single model weights=[a] or weights=a
+    if not map_location:
+        map_location = model.device
+    for w in weights if isinstance(weights, list) else [weights]:
+        attempt_download(w)
+        model.append(torch.load(w, map_location=map_location)['model'].float().fuse().eval())  # load FP32 model
+
+    # Compatibility updates
+    for m in model.modules():
+        if type(m) in [nn.Hardswish, nn.LeakyReLU, nn.ReLU, nn.ReLU6]:
+            m.inplace = True  # pytorch 1.7.0 compatibility
+        elif type(m) is Conv:
+            m._non_persistent_buffers_set = set()  # pytorch 1.6.0 compatibility
+
+    if len(model) == 1:
+        return model[-1]  # return model
+    else:
+        print('Ensemble created with %s\n' % weights)
+        for k in ['names', 'stride']:
+            setattr(model, k, getattr(model[-1], k))
+        return model
+
+
+def Load_Yolo_Model(device=select_device(''), weights='models/yolov5s.pt', imgsz=640):
+    """Save a yolo model object.
+
+    Args:
+        device (device): Which device to use, cpu or gpu.
+        weights (String): Relative location of weights file.
+
+    Returns:
+        model: yolov5 model.
+    """
+    yolov5 = attempt_load_yolov5(weights, map_location=device)
+    yolov5.device = device
+    yolov5.weights = weights
+    # half precision only supported on CUDA
+    yolov5.is_half = yolov5.device.type != 'cpu'
+    if yolov5.is_half:
+        yolov5.half()
+    yolov5.imgsz = check_img_size(imgsz, s=yolov5.stride.max())
+    yolov5.names = (yolov5.module.names if hasattr(yolov5, 'module') else yolov5.names)
+    yolov5.colors = ([[random.randint(0, 255) for _ in range(3)] for _ in range(len(yolov5.names))])
+    return yolov5
+
+
+def initialize_video(source, webcam, imgsz):
+    if webcam:
+        #view_img = True
+        cudnn.benchmark = True
+        vid = LoadStreams(source, img_size=imgsz)
+    else:
+        #save_img = True
+        vid = LoadImages(source, img_size=imgsz)
+    return vid
+
+
+def get_frame(vid):
+    return next(vid)
+
+
+def xyxy2topleftwh(x):
+    # Convert nx4 boxes from [x1, y1, x2, y2] to [x, y, w, h] where xy1=top-left, xy2=bottom-right
+    y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else zeros_like(x)
+    y[:, 0] = x[:, 0]  # top left x
+    y[:, 1] = x[:, 1]  # top left y
+    y[:, 2] = x[:, 2] - x[:, 0]  # width
+    y[:, 3] = x[:, 3] - x[:, 1]  # height
+    return y
+
+
+def yolo_predict(yolov5, path, img, im0s, vid_cap, out, dataset, vid_path, vid_writer, view_img=True, save_img=True, save_txt=False, webcam=False, augment=False, conf_thres=0.25, classes=None, agnostic_nms=False):
+    img = torch.from_numpy(img).to(yolov5.device)
+    img = img.half() if yolov5.is_half else img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+
+    # Inference
+    t1 = time_synchronized()
+    pred = yolov5(img, augment=augment)[0]
+
+    # Apply NMS
+    pred = non_max_suppression(
+        pred, opt.conf_thres, opt.iou_thres, classes=classes, agnostic=agnostic_nms)
+    t2 = time_synchronized()
+
+    boxes, names, scores = [], [], []
+
+    # Process detections
+    for i, det in enumerate(pred):  # detections per image
+        if webcam:  # batch_size >= 1
+            p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
+        else:
+            p, s, im0 = path, '', im0s
+
+        save_path = str(Path(out) / Path(p).name)
+        txt_path = str(Path(out) / Path(p).stem) + ('_%g' % dataset.frame if dataset.mode == 'video' else '')
+        s += '%gx%g ' % img.shape[2:]  # print string
+        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        if det is not None and len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(
+                img.shape[2:], det[:, :4], im0.shape).round()
+
+            # Print results
+            for c in det[:, -1].unique():
+                n = (det[:, -1] == c).sum()  # detections per class
+                s += '%g %ss, ' % (n, yolov5.names[int(c)])  # add to string
+
+            # Write results
+            for *xyxy, conf, cls in reversed(det):
+                if save_txt:  # Write to file
+                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) /
+                            gn).view(-1).tolist()  # normalized xywh
+                    xywh_deepsort = (xyxy2topleftwh(torch.tensor(xyxy).view(1, 4))).view(-1).tolist()  # xywh
+                    # label format
+                    line = (cls, conf, *xywh_deepsort) if opt.save_conf else (cls, *xywh_deepsort)
+                    with open(txt_path + '.txt', 'a') as f:
+                        f.write(('%g ' * len(line) + '\n') % line)
+
+                if save_img or view_img:  # Add bbox to image
+                    label = '%s %.2f' % (yolov5.names[int(cls)], conf)
+                    plot_one_box(xyxy, im0, label=label,
+                                 color=yolov5.colors[int(cls)], line_thickness=3)
+
+        # Print time (inference + NMS)
+        print('%sDone. (%.3fs)' % (s, t2 - t1))
+
+        # Stream results
+        stream_result(view_img, p, im0, save_img, dataset, save_path, vid_path, vid_writer, vid_cap)
+
+    return boxes, names, scores
+
+
+def stream_result(view_img, p, im0, save_img, dataset, save_path, vid_path, vid_writer, vid_cap):
+    # Stream results
+    if view_img:
+        cv2.imshow(p, im0)
+        if cv2.waitKey(1) == ord('q'):  # q to quit
+            raise StopIteration
+
+    # Save results (image with detections)
+    if save_img:
+        if dataset.mode == 'images':
+            cv2.imwrite(save_path, im0)
+        else:
+            if vid_path != save_path:  # new video
+                vid_path = save_path
+                if isinstance(vid_writer, cv2.VideoWriter):
+                    vid_writer.release()  # release previous video writer
+
+                fourcc = 'mp4v'  # output video codec
+                fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                vid_writer = cv2.VideoWriter(
+                    save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
+            vid_writer.write(im0)
+
+
+def detect(save_img=False):
+    out, source, weights, view_img, save_txt, imgsz = \
+        opt.save_dir, opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
+    webcam = source.isnumeric() or source.startswith(
+        ('rtsp://', 'rtmp://', 'http://')) or source.endswith('.txt')
+
+    # Initialize
+    set_logging()
+    device = select_device(opt.device)
+    if os.path.exists(out):  # output dir
+        shutil.rmtree(out)  # delete dir
+    os.makedirs(out)  # make new dir
+
+    # Load model
+    yolov5 = Load_Yolo_Model(device=device, weights=weights, imgsz=imgsz)
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    if webcam:
+        view_img = True
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        dataset = LoadStreams(source, img_size=yolov5.imgsz)
+    else:
+        save_img = True
+        dataset = LoadImages(source, img_size=imgsz)
+
+    # Run inference
+    t0 = time.time()
+    img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
+    _ = yolov5(img.half() if yolov5.is_half else img) if yolov5.is_half else None
+
+    for path, img, im0s, vid_cap in dataset:
+        yolo_predict(yolov5, path, img, im0s, vid_cap, out, dataset, vid_path, vid_writer, save_txt=save_txt)
+
+    if save_txt or save_img:
+        print('Results saved to %s' % Path(out))
+
+    print('Done. (%.3fs)' % (time.time() - t0))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str,
+                        default='yolov5s.pt', help='model.pt path(s)')
+    # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str,
+                        default='inference/images', help='source')
+    parser.add_argument('--img-size', type=int, default=640,
+                        help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float,
+                        default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float,
+                        default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='',
+                        help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true',
+                        help='display results')
+    parser.add_argument('--save-txt', action='store_true',
+                        help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true',
+                        help='save confidences in --save-txt labels')
+    parser.add_argument('--save-dir', type=str,
+                        default='inference/output', help='directory to save results')
+    parser.add_argument('--classes', nargs='+', type=int,
+                        help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true',
+                        help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true',
+                        help='augmented inference')
+    parser.add_argument('--update', action='store_true',
+                        help='update all models')
+    opt = parser.parse_args()
+    print(opt)
+
+    with torch.no_grad():
+        if opt.update:  # update all models (to fix SourceChangeWarning)
+            for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']:
+                detect()
+                strip_optimizer(opt.weights)
+        else:
+            detect()
