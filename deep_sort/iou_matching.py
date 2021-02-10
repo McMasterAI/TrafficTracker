118abc7abdc0a379e27141fea2bba058b9325560
muhammad ali <muhammadaliog3@live.com>
2021-02-10 10:56:56 -0500

    Adding numba to iou_matching.py

--- deep_sort/iou_matching.py
+++ deep_sort/iou_matching.py
@@ -2,9 +2,12 @@
 from __future__ import absolute_import
 import numpy as np
 from . import linear_assignment
-from numba import njit,jit
+from numba import njit,jit, vectorize
 
+def numba_prod(arr):
+    return np.product(arr)
 
+@njit
 def iou(bbox, candidates):
     """Computer intersection over union.
 
@@ -28,15 +31,16 @@ def iou(bbox, candidates):
     candidates_tl = candidates[:, :2]
     candidates_br = candidates[:, :2] + candidates[:, 2:]
 
-    tl = np.c_[np.maximum(bbox_tl[0], candidates_tl[:, 0])[:, np.newaxis],
-               np.maximum(bbox_tl[1], candidates_tl[:, 1])[:, np.newaxis]]
-    br = np.c_[np.minimum(bbox_br[0], candidates_br[:, 0])[:, np.newaxis],
-               np.minimum(bbox_br[1], candidates_br[:, 1])[:, np.newaxis]]
+    tl = np.concatenate((np.expand_dims(np.maximum(bbox_tl[0], candidates_tl[:, 0]),axis=1),
+               np.expand_dims(np.maximum(bbox_tl[1], candidates_tl[:, 1]), axis=1 )), axis=1)
+    br = np.concatenate((np.expand_dims(np.minimum(bbox_br[0], candidates_br[:, 0]),axis=1),
+               np.expand_dims(np.minimum(bbox_br[1], candidates_br[:, 1]), axis=1 )),axis=1)
     wh = np.maximum(0., br - tl)
 
-    area_intersection = wh.prod(axis=1)
+    area_intersection = wh[:,0]*wh[:,1]
     area_bbox = bbox[2:].prod()
-    area_candidates = candidates[:, 2:].prod(axis=1)
+    area_candidates = candidates[:, 2:]
+    area_candidates = area_candidates[:,0]*area_candidates[:,1]
     return area_intersection / (area_bbox + area_candidates - area_intersection)
 
 
