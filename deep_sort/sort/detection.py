28a6883810ce95db73b3b98dcc113ce0f1ad864c
Muhammad Ali <muhammadaliog3@live.com>
2021-03-02 19:00:33 -0500

    Adding class_name storage to deep_sort

--- deep_sort/sort/detection.py
+++ deep_sort/sort/detection.py
@@ -26,10 +26,14 @@ class Detection(object):
 
     """
 
-    def __init__(self, tlwh, confidence, feature):
+    def __init__(self, tlwh, confidence, class_name, feature):
         self.tlwh = np.asarray(tlwh, dtype=np.float)
         self.confidence = float(confidence)
+        self.class_name = class_name
         self.feature = np.asarray(feature, dtype=np.float32)
+
+    def get_class(self):
+        return self.class_name
 
     def to_tlbr(self):
         """Convert bounding box to format `(min x, min y, max x, max y)`, i.e.,
