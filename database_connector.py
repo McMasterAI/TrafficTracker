a09af82c598d71edc6f188f826835f4baaa233fa
Muhammad Ali <muhammadaliog3@live.com>
2021-03-20 09:37:40 -0400

    Adding pandas dataframe as a valid format

--- database_connector.py
+++ database_connector.py
@@ -1,4 +1,6 @@
 import pyodbc
+import pandas as pd
+
 server = 'traffictracker.database.windows.net'
 database = 'heatmap'
 username = 'macadmin'
@@ -9,7 +11,7 @@ connection_line= 'DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+datab
 heatmap_default_column_order = ['heatmap_id', 'created_time', 'Pos_x', 'Pos_y', 
     'width', 'height', 'Class', 'Object_id', 'locaton_id']
 location_default_column_order = ['location_id', 'latitude','longitude']
-table_default_column_order = {
+default_column_order = {
     'heatmap':heatmap_default_column_order,
     'locations':location_default_column_order
 }
@@ -21,6 +23,7 @@ def execute_raw_query(query):
             return cursor.execute(query)
 
 def query_table(select_clause, where_clause, table, format='tuple'):
+    columns = default_column_order[table] if select_clause == ['*'] else select_clause
     select_clause = ','.join(select_clause)
     where_clause = ','.join(where_clause)
     query = 'SELECT ' + select_clause + ' FROM '+table
@@ -29,16 +32,19 @@ def query_table(select_clause, where_clause, table, format='tuple'):
     print('Querying: ',query)
     with pyodbc.connect(connection_line) as conn:
         with conn.cursor() as cursor:
-            if format=='tuple':
+            if format == 'tuple':
                 result = [tuple(map(str.strip, row)) for row in cursor.execute(query)] #strips every attribute in every record
-            elif format=='raw':
+            elif format == 'raw':
                 result = cursor.execute(query).fetchall()
                 print(type(result))
+            elif format == 'dataframe':
+                result = [tuple(map(str.strip, row)) for row in cursor.execute(query)]
+                result = pd.DataFrame( result , columns=columns)
             else:
                 raise('Query output format does not exist')
             return result
 
-print(query_table(['*'],[],'locations', format='tuple'))
+print(query_table(['location_id','longitude'],[],'locations', format='dataframe'))
 for r in query_table(['*'],[],'locations', format='raw'):
     print(r.location_id.strip(), r.longitude.strip())
 
@@ -55,31 +61,3 @@ def describe_query_table(select_clause, table):
 
 print(describe_query_table(['*'],'heatmap'))
 
-def insert_table(table, records, column_order=None):
-    if column_order==None:
-        column_order = table_default_column_order[table]
-    into_clause = table + '(' + ','.join(column_order) + ')'
-    values_clause = '( ' + ','.join(['?']*len(column_order)) + ' )'
-    query = "INSERT INTO " + into_clause + " VALUES "+values_clause
-    print('Querying: ',query)
-    with pyodbc.connect(connection_line) as conn:
-        with conn.cursor() as cursor:
-            try:
-                conn.autocommit = False
-                cursor.fast_executemany = True
-                """
-                Under the hood, there is one important difference when fast_executemany=True. In that case, on the client side, pyodbc converts the Python parameter values to their ODBC "C" equivalents, based on the target column types in the database. 
-                E.g., a string-based date parameter value of "2018-07-04" is converted to a C date type binary value by pyodbc before sending it to the database. 
-                When fast_executemany=False, that date string is sent as-is to the database and the database does the conversion. This can lead to some subtle differences in behavior depending on whether fast_executemany is True or False.
-                """
-                cursor.executemany(query, records)
-            except pyodbc.DatabaseError as err:
-                print('DATABASE ERROR', err)
-                conn.rollback()
-            else:
-                conn.commit()
-                print('Insertion successful')
-            finally:
-                conn.autocommit = True
-
-insert_table('heatmap', [(1, "2021-03-18", 5,5,5,5,'nothing', 1,'Mcmaster University')])
